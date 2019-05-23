59a69888df4a9da61ecc712e4861bd6231571d0a
megaturbo <megaturbo@truelevel.io>
2019-05-23 12:04:07 +0200

    Use handlebars to render shows list

--- app/js/plome.js
+++ app/js/plome.js
@@ -1,20 +1,44 @@
+// shitty code incoming TODO: better
+
+// global vars and datas
+// ============================
 var r = [3, 3, 3, 2, 3, 3, 3, 3, 2, 3, 2, 2];
 var r_i = -1;   // index
 var r_bpm = 95;
 var r_rate = 0.25;
 
-var p_color = '#303030'
+var p_color = '#b0b0b0';
+let SHOWS;
 
-function random_hue(){
-    return 'rgb(' + Math.floor(255*Math.random()) + ','
-    + Math.floor(255*Math.random()) + ','
-    + Math.floor(255*Math.random()) + ')';
+// functions wow
+// ============================
+function plome() {
+  showShows();
 }
+
+function showShows() {
+  // load json
+  $.getJSON("js/shows.json", (shows) => {
+    // compute past status
+    const today = Date.now();
+    shows.shows.map(show => {
+      show.past = today > Date.parse(show.date);
+      return show;
+    });
+
+    // compile and render shows template
+    let source = $('#template-dates').html();
+    let template = Handlebars.compile(source);
+    let rendered = template(shows);
+    $('#target-dates').html(rendered);
+  })
+}
+
 
 function glow_title(){
     var p = $('#plome-title')
     p.css('color', p_color)
-    p.css('font-size', '12em')
+    p.css('font-size', '10em')
 
     p.animate({
         color: '#f2f2f2',
@@ -43,8 +67,10 @@ $(document).on('click', 'a[href^="#"]', function(event){
 make_it_glow();
 
 
+// main shit
 $(function() {
   $(window).load(function() {
+    plome();
     console.log("🥒'ed by @nichuguen & @megaturbo");
   });
 });
